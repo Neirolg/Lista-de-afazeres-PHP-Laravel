26207585f06019236f9e4674b4f34a5ce83b56e1
KelvinKamchen <kelvin.kamchen@hotmail.com>
2022-09-15 17:14:24 -0300

    update

--- ToDoList/resources/views/home/about.blade.php
+++ ToDoList/resources/views/home/about.blade.php
@@ -1,7 +1,7 @@
-<!-- @extends('layouts.app') -->
-<!-- @section('title', $viewData["title"])
+@extends('layouts.app')
+@section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
-@section('content') -->
+@section('content')
 
 <html>
 <script type="module" src="https://md-block.verou.me/md-block.js"></script>
