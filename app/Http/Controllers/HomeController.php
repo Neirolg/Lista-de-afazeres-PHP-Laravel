8031c2fe1300efb96369622424a7a01176040972
KelvinKamchen <kelvin.kamchen@hotmail.com>
2022-10-18 14:16:01 -0300

    'sobre' funcionando sem login 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-
-
+    
     /**
      * Show the application dashboard.
      *
