5bd091b834f20e353a1d046db03e582d070631ca
KelvinKamchen <kelvin.kamchen@hotmail.com>
2022-10-19 14:24:45 -0300

    largura caixa login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,12 +20,12 @@
             
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-min" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Senha') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-min" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
