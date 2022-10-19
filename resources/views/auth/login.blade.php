dcaa55c489b3f6ef9e1735c6c2e7bd28fe8df578
KelvinKamchen <kelvin.kamchen@hotmail.com>
2022-10-19 14:14:37 -0300

    teste4 botao 'entrar'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                 @endif
 
                 <div class="form-group row mb-0">
-                            <div class="col-md-8">
+                            <div class="col-md-8 offset-md-2 text-black">
                                 <button type="submit" class="btn btn-dark">
                                     Entrar
                                 </button>
