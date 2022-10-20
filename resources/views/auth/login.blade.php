7a9e118421efdec6d84ec67dd444831401e56170
KelvinKamchen <kelvin.kamchen@hotmail.com>
2022-10-20 12:19:22 -0300

    ajuste para tela celular

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
         @endif
     
 
-        <form method="POST" action="{{ route('login') }}">
+        <form class="w-full" method="POST" action="{{ route('login') }}">
             @csrf
             
             <div>
